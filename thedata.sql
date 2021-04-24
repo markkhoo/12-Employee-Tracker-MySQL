f6f470cf540f65520a43c3d22a4ef77414c22f69
markeezy <markkhoo95@gmail.com>
2021-04-23 17:49:21 -0700

    add base files and install modules

---
+++ thedata.sql
@@ -0,0 +1,36 @@
+DROP DATABASE IF EXISTS company_DB;
+CREATE DATABASE company_DB;
+USE company_DB;
+
+-- *************************** CREATE QUERIES ***************************
+
+-- CREATE TABLE department
+CREATE TABLE department (
+    id INT AUTO_INCREMENT NOT NULL,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+-- CREATE TABLE roles
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DEC DEFAULT 0,
+    PRIMARY KEY (id),
+    -- FOREIGN KEY CONSTRAINTS
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+-- CREATE TABLE employee
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    -- FOREIGN KEY CONSTRAINTS
+    manager_id INT NOT NULL,
+    roles_id INT NOT NULL,
+    FOREIGN KEY (manager_id) REFERENCES employee(id),
+    FOREIGN KEY (roles_id) REFERENCES roles(id)
+);
+
